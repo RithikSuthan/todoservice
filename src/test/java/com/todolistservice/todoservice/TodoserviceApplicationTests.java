12d1ca7381bbcad239bac7f4ef166f5a2f8dc72f
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-01-05 19:46:16 +0530

    Initial commit

---
+++ src/test/java/com/todolistservice/todoservice/TodoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolistservice.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
