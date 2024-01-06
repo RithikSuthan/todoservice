0fd3d0d8081b5ff3397cdee62c2f50a7fa4abf2c
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-01-06 09:29:32 +0530

    added addplan service

--- src/main/java/com/todolistservice/todoservice/service/newPlanService.java
+++ src/main/java/com/todolistservice/todoservice/service/newPlanService.java
@@ -1,14 +1,27 @@
 package com.todolistservice.todoservice.service;
 
 import com.todolistservice.todoservice.model.newPlan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class newPlanService {
+    @Autowired
+    MongoTemplate mongoTemplate;
     public ResponseEntity<?>addPlan(newPlan plan)
     {
+        if (plan.getPlan()!=null)
+        {
+            plan.setStatus(false);
+            mongoTemplate.save(plan);
+        }
+        else
+        {
+            return ResponseEntity.ok("Plan is empty");
+        }
         return ResponseEntity.ok("Data added Successfully");
     }
 }
